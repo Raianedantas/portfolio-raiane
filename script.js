ad3eb542f054a9fa84cc3b330a3bf8b4a5b294bd
Raianedantas <112406065+Raianedantas@users.noreply.github.com>
2023-05-28 14:07:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready(function(){
+  $("#botao1").click(function(){
+    $("#mais1").toggleClass("esconder");
+  });
+});
+
+$(document).ready(function(){
+  $("#botao2").click(function(){
+    $("#mais2").toggleClass("esconder");
+  });
+});
+
+$(document).ready(function(){
+  $("#botao3").click(function(){
+    $("#mais3").toggleClass("esconder");
+  });
+});
+
+$(document).ready(function(){
+  $("#botao4").click(function(){
+    $("#mais4").toggleClass("esconder");
+  });
+});
+$(document).ready(function(){
+  $("#botao5").click(function(){
+    $("#mais5").toggleClass("esconder");
+  });
+});
